385ec2f2e3fbcd9699484ae3c0f25ff81af018b1
Саша O. <si.ovcharuk2@gmail.com>
2026-04-13 14:16:41 +0300

    Modified common.h file

--- Sea Battle/common.h
+++ Sea Battle/common.h
@@ -1,6 +1,10 @@
 #pragma once
 
 const int BOARD_SIZE = 10;
+
+const int TOTAL_SHIPS = 10;
+const int SHIP_TYPES[TOTAL_SHIPS] = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
+
 enum CellState { EMPTY = '~', SHIP = 'S', HIT = 'X', MISS = '*' };
 enum BotState { SEARCH, HUNT };
 
