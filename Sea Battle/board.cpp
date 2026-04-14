3b381869902033b847cd85d35b1e426111b83fa9
Саша O. <si.ovcharuk2@gmail.com>
2026-04-14 18:00:54 +0300

    Fixed board.cpp file

--- Sea Battle/board.cpp
+++ Sea Battle/board.cpp
@@ -14,19 +14,19 @@ void initBoard(char board[BOARD_SIZE][BOARD_SIZE])
 
 void printBoard(char pBoard[BOARD_SIZE][BOARD_SIZE], char cBoard[BOARD_SIZE][BOARD_SIZE])
 {
-	std::cout << "    ГРАВЕЦЬ              БОТ" << std::endl;
-	std::cout << "  0 1 2 3 4 5 6 7 8 9    0 1 2 3 4 5 6 7 8 9" << std::endl;
+	std::cout << "ГРАВЕЦЬ                   БОТ" << std::endl;
+	std::cout << "  0 1 2 3 4 5 6 7 8 9       0 1 2 3 4 5 6 7 8 9" << std::endl;
 
 	for (int i = 0; i < BOARD_SIZE; i++)
 	{
 		std::cout << i << " ";
 		for (int j = 0; j < BOARD_SIZE; j++)
-			std::cout << pBoard << " ";
-		std::cout << "  " << i << " ";
+			std::cout << (char)pBoard[i][j] << " ";
+		std::cout << "    " << i << " ";
 		for (int j = 0; j < BOARD_SIZE; j++)
 		{
-			if (cBoard[i][j] == SHIP) std::cout << EMPTY << " ";
-			else std::cout << cBoard[i][j] << " ";
+			if (cBoard[i][j] == SHIP) std::cout << (char)EMPTY << " ";
+			else std::cout << (char)cBoard[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
